fc582e612c609ef53448937f6f21b3428653b34f
Eduardo Macarron <eduardo.macarron@gmail.com>
2013-04-03 10:13:51 +0200

    Wagon: Deploying site to repository

--- apidocs/reference/lists.js
+++ apidocs/reference/lists.js
@@ -3,16 +3,17 @@ var DATA = [
       { id:1, label:"org.mybatis.scripting.velocity.Driver", link:"org/mybatis/scripting/velocity/Driver.html", type:"class" },
       { id:2, label:"org.mybatis.scripting.velocity.FastLinkedList", link:"org/mybatis/scripting/velocity/FastLinkedList.html", type:"class" },
       { id:3, label:"org.mybatis.scripting.velocity.FastLinkedList.Node", link:"org/mybatis/scripting/velocity/FastLinkedList.Node.html", type:"class" },
-      { id:4, label:"org.mybatis.scripting.velocity.ParameterMappingCollector", link:"org/mybatis/scripting/velocity/ParameterMappingCollector.html", type:"class" },
-      { id:5, label:"org.mybatis.scripting.velocity.ParameterMappingSourceParser", link:"org/mybatis/scripting/velocity/ParameterMappingSourceParser.html", type:"class" },
-      { id:6, label:"org.mybatis.scripting.velocity.RepeatDirective", link:"org/mybatis/scripting/velocity/RepeatDirective.html", type:"class" },
-      { id:7, label:"org.mybatis.scripting.velocity.RepeatDirective.NullHolderContext", link:"org/mybatis/scripting/velocity/RepeatDirective.NullHolderContext.html", type:"class" },
-      { id:8, label:"org.mybatis.scripting.velocity.RepeatDirective.RepeatScope", link:"org/mybatis/scripting/velocity/RepeatDirective.RepeatScope.html", type:"class" },
-      { id:9, label:"org.mybatis.scripting.velocity.SQLScriptSource", link:"org/mybatis/scripting/velocity/SQLScriptSource.html", type:"class" },
-      { id:10, label:"org.mybatis.scripting.velocity.SetDirective", link:"org/mybatis/scripting/velocity/SetDirective.html", type:"class" },
-      { id:11, label:"org.mybatis.scripting.velocity.TrimDirective", link:"org/mybatis/scripting/velocity/TrimDirective.html", type:"class" },
-      { id:12, label:"org.mybatis.scripting.velocity.TrimDirective.Params", link:"org/mybatis/scripting/velocity/TrimDirective.Params.html", type:"class" },
-      { id:13, label:"org.mybatis.scripting.velocity.VelocityFacade", link:"org/mybatis/scripting/velocity/VelocityFacade.html", type:"class" },
-      { id:14, label:"org.mybatis.scripting.velocity.WhereDirective", link:"org/mybatis/scripting/velocity/WhereDirective.html", type:"class" }
+      { id:4, label:"org.mybatis.scripting.velocity.InDirective", link:"org/mybatis/scripting/velocity/InDirective.html", type:"class" },
+      { id:5, label:"org.mybatis.scripting.velocity.ParameterMappingCollector", link:"org/mybatis/scripting/velocity/ParameterMappingCollector.html", type:"class" },
+      { id:6, label:"org.mybatis.scripting.velocity.ParameterMappingSourceParser", link:"org/mybatis/scripting/velocity/ParameterMappingSourceParser.html", type:"class" },
+      { id:7, label:"org.mybatis.scripting.velocity.RepeatDirective", link:"org/mybatis/scripting/velocity/RepeatDirective.html", type:"class" },
+      { id:8, label:"org.mybatis.scripting.velocity.RepeatDirective.NullHolderContext", link:"org/mybatis/scripting/velocity/RepeatDirective.NullHolderContext.html", type:"class" },
+      { id:9, label:"org.mybatis.scripting.velocity.RepeatDirective.RepeatScope", link:"org/mybatis/scripting/velocity/RepeatDirective.RepeatScope.html", type:"class" },
+      { id:10, label:"org.mybatis.scripting.velocity.SQLScriptSource", link:"org/mybatis/scripting/velocity/SQLScriptSource.html", type:"class" },
+      { id:11, label:"org.mybatis.scripting.velocity.SetDirective", link:"org/mybatis/scripting/velocity/SetDirective.html", type:"class" },
+      { id:12, label:"org.mybatis.scripting.velocity.TrimDirective", link:"org/mybatis/scripting/velocity/TrimDirective.html", type:"class" },
+      { id:13, label:"org.mybatis.scripting.velocity.TrimDirective.Params", link:"org/mybatis/scripting/velocity/TrimDirective.Params.html", type:"class" },
+      { id:14, label:"org.mybatis.scripting.velocity.VelocityFacade", link:"org/mybatis/scripting/velocity/VelocityFacade.html", type:"class" },
+      { id:15, label:"org.mybatis.scripting.velocity.WhereDirective", link:"org/mybatis/scripting/velocity/WhereDirective.html", type:"class" }
 
     ];
