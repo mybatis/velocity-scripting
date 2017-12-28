3d22b968da35e21e0838c8da029e67410a7facb9
Jeremy Landis <jeremylandis@hotmail.com>
2017-12-28 01:54:30 -0500

    [ci] Remove annotated author already covered by license (blame the code not the author)

--- src/test/java/org/mybatis/scripting/velocity/use/EnumWrapper.java
+++ src/test/java/org/mybatis/scripting/velocity/use/EnumWrapper.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2012-2016 the original author or authors.
+ *    Copyright 2012-2017 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -17,10 +17,6 @@ package org.mybatis.scripting.velocity.use;
 
 import java.util.HashMap;
 
-/**
- *
- * @author mnesarco
- */
 public class EnumWrapper extends HashMap<String, Integer> {
 
   private static final long serialVersionUID = 1L;
