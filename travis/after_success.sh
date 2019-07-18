4174046248faaa65348806194abd38bb67a01b02
Kazuki Shimizu <kazuki43zoo@gmail.com>
2019-07-19 03:38:38 +0900

    Change to use openjdk on Travis CI

--- travis/after_success.sh
+++ travis/after_success.sh
@@ -33,7 +33,7 @@ echo "Current commit detected: ${commit_message}"
 
 if [ $TRAVIS_REPO_SLUG == "mybatis/velocity-scripting" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ] && [[ "$commit_message" != *"[maven-release-plugin]"* ]]; then
 
-  if [ $TRAVIS_JDK_VERSION == "oraclejdk8" ]; then
+  if [ $TRAVIS_JDK_VERSION == "openjdk8" ]; then
     # Deploy to sonatype
     ./mvnw clean deploy -q --settings ./travis/settings.xml
     echo -e "Successfully deployed SNAPSHOT artifacts to Sonatype under Travis job ${TRAVIS_JOB_NUMBER}"
