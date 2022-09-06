b594b6ad9af0abb1016de2a2606ec7c5f406dd25
Jeremy Landis <jeremylandis@hotmail.com>
2022-09-05 21:33:05 -0400

    [secure] Use secure site and formatting

--- src/test/java/org/mybatis/scripting/velocity/use/VelocityLanguageTest.java
+++ src/test/java/org/mybatis/scripting/velocity/use/VelocityLanguageTest.java
@@ -1,11 +1,11 @@
 /*
- *    Copyright 2012-2021 the original author or authors.
+ *    Copyright 2012-2022 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
  *    You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *       https://www.apache.org/licenses/LICENSE-2.0
  *
  *    Unless required by applicable law or agreed to in writing, software
  *    distributed under the License is distributed on an "AS IS" BASIS,
@@ -44,12 +44,7 @@ class VelocityLanguageTest {
 
   @SuppressWarnings("unused")
   public enum IDS {
-    ZERO,
-    ONE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE
+    ZERO, ONE, TWO, THREE, FOUR, FIVE
   }
 
   @BeforeAll
